5bad16668da237dc79b7e2dc8c73c1ae5a765677
Avital Aviv <avitalos6@gmail.com>
2022-05-31 20:23:20 +0300

    created front end sign in activity

--- app/src/main/java/talktome/com/MainActivity.java
+++ app/src/main/java/talktome/com/MainActivity.java
@@ -1,8 +1,12 @@
 package talktome.com;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // login button handler
+        Button btn_login = findViewById(R.id.login_btn);
+        btn_login.setOnClickListener(v -> {
+            //need to validate the user name + password
+            Intent i = new Intent(this, ContactsChatActivity.class);
+            startActivity(i);
+        });
     }
 }
