21801a9199c0700a239b93227f1e2183003d3f21
Avital Aviv <avitalos6@gmail.com>
2022-06-02 13:45:33 +0300

    Register page

--- app/src/main/java/talktome/com/MainActivity.java
+++ app/src/main/java/talktome/com/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // login button handler
         Button btn_login = findViewById(R.id.login_btn);
         btn_login.setOnClickListener(v -> {
-            //need to validate the user name + password
+            EditText userNameInput = (EditText)findViewById(R.id.editTextTextPersonName);
+            String user_name = userNameInput.getText().toString();
+            EditText passwordInput = (EditText)findViewById(R.id.editTextTextPassword);
+            String user_password = passwordInput.getText().toString();
+            
+            //check validation of user name & password (need to send to server side)
             Intent i = new Intent(this, ContactsChatActivity.class);
             startActivity(i);
         });
+
+
+        TextView textView = findViewById(R.id.textView1);
+        String text = "New User? Click here";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            public void onClick(View widget) {
+                Toast.makeText(MainActivity.this, "here", Toast.LENGTH_SHORT).show();
+                Intent a = new Intent(MainActivity.this, RegisterPageActivity.class);
+                startActivity(a);
+            }
+        };
+        ss.setSpan(clickableSpan1, 16, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
--- app/src/main/java/talktome/com/RegisterPageActivity.java
+++ app/src/main/java/talktome/com/RegisterPageActivity.java
@@ -2,7 +2,16 @@ package talktome.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterPageActivity extends AppCompatActivity {
 
@@ -10,5 +19,32 @@ public class RegisterPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        //the click to go back to main activity
+        String text = "Already registered? click here";
+        TextView textView = findViewById(R.id.textView2);
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            public void onClick(View widget) {
+                Toast.makeText(RegisterPageActivity.this, "here", Toast.LENGTH_SHORT).show();
+                Intent a = new Intent(RegisterPageActivity.this, MainActivity.class);
+                startActivity(a);
+            }
+        };
+        ss.setSpan(clickableSpan1, 26, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //register button
+        Button btn_login = findViewById(R.id.register_btn);
+        btn_login.setOnClickListener(v -> {
+            //get user+nick name and passwords - validate all and send to the server
+        });
+
+        //upload photo button
+        Button btn_upload_photo = findViewById(R.id.button_upload_photo);
+        btn_upload_photo.setOnClickListener(v -> {
+
+        });
     }
 }
