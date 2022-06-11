e6ae572bd354d7c999b13d5b3c4a495eac0ed976
Avital Aviv <avitalos6@gmail.com>
2022-06-11 18:34:50 +0300

    created message layouts and the chat messages layout

---
+++ app/src/main/java/talktome/com/ChatMessagesActivity.java
@@ -0,0 +1,13 @@
+package talktome;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import talktome.com.R;
+
+public class ChatMessagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_messages);
+    }
+}
